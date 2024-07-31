ca0cd240a0806ba5db07cd7dd2afa9b623faa54a
Ultraxime <bruno.maxime@yahoo.fr>
2024-07-31 16:45:56 +0200

    Creating the logger

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# log-tester
-Logger for rust to check if the log verify some property
+# log_tester
+
+[![crates.io](https://img.shields.io/crates/v/log_tester.svg)](https://crates.io/crates/log_tester)
+[![Documentation](https://docs.rs/log_tester/badge.svg)](https://docs.rs/log_tester)
+[![License: GNU GPLv3](https://img.shields.io/badge/License-GPLv3-green.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
+**log_tester** is a crate that takes care of capturing log messages produced by the [`log`](https://docs.rs/log) crate during test, and then perform checks on them.
+
+## Usage
+
+This crate is intend to be used in conjunction with the [`log`](https://docs.rs/log)
+ crate. And only during test.
+
+```toml
+[dependencies]
+log = "0.4"
+
+[dev-dependencies]
+log_tester = "0.1"
+```
+
+```rust, ignore
+use log_tester::LogTester;
+use log::Level;
+
+#[test]
+fn test_log() {
+    LogTester::start();
+    log::info!("Hello, world!");
+
+    assert!(LogTester::contains(Level::Info, "Hello, world!"))
+}
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## Authors
+
+- [@Ultraxime](https://github.com/Ultraxime)
+
+## License
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
---
+++ src/lib.rs
@@ -0,0 +1,161 @@
+// @Author: Ultraxime
+//
+// This file is part of Log Tester.
+//
+// Log Tester is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License.
+//
+// Log Tester is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Log Tester. If not, see <https://www.gnu.org/licenses/>.
+
+#![doc = include_str!("../README.md")]
+
+use std::sync::RwLock;
+
+use log::{Level, Log, Record};
+
+/// The list of captured logs.
+static LOGS: RwLock<Vec<CapturedLog>> = RwLock::new(Vec::new());
+
+/// The logger
+pub struct LogTester;
+
+/// A log that was captured
+pub struct CapturedLog {
+    /// The formatted log message.
+    pub body: String,
+    /// The level.
+    pub level: Level,
+    /// The target.
+    pub target: String,
+}
+
+impl LogTester {
+    /// Start the logger
+    ///
+    /// This should only be called once
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use log_tester::LogTester;
+    ///
+    /// LogTester::start();
+    /// ```
+    pub fn start() {
+        log::set_logger(&LogTester).expect("Failed to start the logger");
+        log::set_max_level(log::LevelFilter::Trace);
+    }
+
+    /// Returns true if there is an entry with the given level that contains the given content
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use log_tester::LogTester;
+    /// use log;
+    ///
+    /// LogTester::start();
+    /// log::info!("Hello, world!");
+    /// assert!(LogTester::contains(log::Level::Info, "Hello, world!"));
+    /// ```
+    pub fn contains(level: Level, content: &str) -> bool {
+        let logs = LOGS
+            .read()
+            .expect("Failed to get the read lock on the logs");
+        for log in logs.iter() {
+            if log.level == level && log.body.contains(content) {
+                return true;
+            }
+        }
+        false
+    }
+
+    /// Returns the number of captured logs
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use log_tester::LogTester;
+    /// use log;
+    ///
+    /// LogTester::start();
+    /// log::info!("Hello, world!");
+    /// assert_eq!(LogTester::len(), 1);
+    /// ```
+    #[allow(dead_code)]
+    pub fn len() -> usize {
+        LOGS.read()
+            .expect("Failed to get the read lock on the logs")
+            .len()
+    }
+}
+
+impl Log for LogTester {
+    fn enabled(&self, _: &log::Metadata) -> bool {
+        true
+    }
+
+    fn log(&self, record: &Record) {
+        LOGS.write()
+            .expect("Failed to get the write lock on the logs")
+            .push(record.into());
+    }
+
+    fn flush(&self) {}
+}
+
+impl From<&Record<'_>> for CapturedLog {
+    fn from(record: &Record<'_>) -> Self {
+        CapturedLog {
+            body: record.args().to_string(),
+            level: record.level(),
+            target: record.target().to_string(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use log::{debug, error, info, log_enabled, trace, warn};
+
+    #[test]
+    fn test_log() {
+        LogTester::start();
+        assert_eq!(LogTester::len(), 0);
+        trace!("trace");
+        debug!("debug");
+        info!("info");
+        warn!("warn");
+        error!("error");
+        assert!(LogTester::contains(Level::Trace, "trace"));
+        assert!(LogTester::contains(Level::Debug, "debug"));
+        assert!(LogTester::contains(Level::Info, "info"));
+        assert!(LogTester::contains(Level::Warn, "warn"));
+        assert!(LogTester::contains(Level::Error, "error"));
+        assert!(!LogTester::contains(Level::Info, "nothing"));
+        assert_eq!(LogTester::len(), 5);
+    }
+
+    #[test]
+    fn test_max_level() {
+        LogTester::start();
+        assert_eq!(log::max_level(), log::LevelFilter::Trace);
+        for level in Level::iter() {
+            assert!(log_enabled!(level));
+        }
+    }
+
+    #[test]
+    fn test_flush() {
+        LogTester::start();
+        log::logger().flush();
+    }
+}
